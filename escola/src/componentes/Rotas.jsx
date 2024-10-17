b40f424c1c141d52b6900b377678fa2e149212bb
Matheus62 <matheusdoni55@gmail.com>
2024-10-17 12:26:05 -0300

    17/10 2°

---
+++ escola/src/componentes/Rotas.jsx
@@ -0,0 +1,17 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Home from "./paginas/home";
+import Cadastro from "./paginas/cadastro";
+import Consulta from "./paginas/consulta";
+import Alteracao from "./paginas/alteracao";
+import Exclusao from "./paginas/exclusao";
+import Erro from "./paginas/erro";
+
+export default function Rotas()
+{
+    return
+    (
+        
+
+    );
+}
